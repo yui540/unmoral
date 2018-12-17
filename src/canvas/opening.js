5430048304e4556f91bf6bc763adbee8d6ce9fad
yui540 <tabletennis540@gmail.com>
2018-12-17 19:53:43 +0900

    add: 演出で使うエフェクトのcanvasを追加

---
+++ src/canvas/opening.js
@@ -0,0 +1,36 @@
+const colors = ['#f04225', '#7dd7e4', '#7dff5c']
+let canvas = null
+let ctx = null
+let timer = null
+
+export const startAnimation = _canvas => {
+  canvas = _canvas
+  ctx = canvas.getContext('2d')
+
+  canvas.width = document.documentElement.clientWidth
+  canvas.height = document.documentElement.clientHeight
+  ;(function tick() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ;[...Array(6).keys()].forEach(i => {
+      const width = (canvas.width / 4) * Math.random()
+      const height = 3 * Math.random()
+      const x = canvas.width * Math.random()
+      const y = canvas.height * Math.random()
+
+      ctx.fillStyle = colors[Math.round(colors.length * Math.random())]
+      ctx.beginPath()
+      ctx.rect(x, y, width, height)
+      ctx.closePath()
+      ctx.fill()
+    })
+
+    timer = requestAnimationFrame(tick)
+  })()
+}
+
+export const stopAnimation = () => {
+  cancelAnimationFrame(timer)
+  timer = null
+  canvas = null
+  ctx = null
+}
