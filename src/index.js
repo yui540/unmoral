0bd69e85c9ef07466f9e104801c32da1cf868a4f
yui540 <tabletennis540@gmail.com>
2018-12-17 14:14:52 +0900

    fix: createjsは使わない方針で。

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import Unmoral from './components/Unmoral'
-import createjs from 'createjs'
-
-console.log(createjs)
 
 const store = configureStore()
 
