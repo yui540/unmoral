9804774160b434827e31e03a979f25cfae4452db
yui540 <tabletennis540@gmail.com>
2018-12-20 22:41:39 +0900

    fix: キャラ画像の差し替え

--- src/config/chara.js
+++ src/config/chara.js
@@ -2,7 +2,7 @@ export default {
   mitsuki: {
     name: './images/character/name/mitsuki.png',
     visual: './images/character/mitsuki.png',
-    pic: './images/character/pic/mitsuki.jpg',
+    pic: './images/character/pic/mitsuki.png',
     profile: `笑顔がまぶしい明朗快活な少女。
     運動神経がずば抜けている。
     ロロを魔改造しちゃうくらいには頭も良い。
@@ -13,7 +13,7 @@ export default {
   lolo: {
     name: './images/character/name/lolo.png',
     visual: './images/character/lolo.png',
-    pic: './images/character/pic/lolo.jpg',
+    pic: './images/character/pic/lolo.png',
     profile: `元々は、子供用の玩具として三姫に与えられたロボットであるが、深い興味を抱いた三姫に年々魔改造されていった。
     三姫を公私共にバックアップするかけがえのない相棒。`,
     prev: 'mitsuki',
@@ -22,7 +22,7 @@ export default {
   amor: {
     name: './images/character/name/amor.png',
     visual: './images/character/amor.png',
-    pic: './images/character/pic/amor.jpg',
+    pic: './images/character/pic/amor.png',
     profile: `都市管理を担う一方でいつしか自我が生成され、人類が大好きになる。
     しかし、その愛情表現がかなりヤンデレ的な思考だったので、人間からは反感を買い、最終的には主人公に諭される。
     自分が正しいと判断したものには強い自信があり、それをもって今回のシナリオが展開される。`,
