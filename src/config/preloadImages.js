2cfaee0e83c45d6838e38e658ab0c36c84fe22bb
yui540 <tabletennis540@gmail.com>
2018-12-21 11:42:04 +0900

    fix: プリロードリストを減らす

--- src/config/preloadImages.js
+++ src/config/preloadImages.js
@@ -20,27 +20,4 @@ export default [
   './images/main__sp.png',
   './images/sub__sp.png',
   './images/message.png',
-  './images/bg-mitsuki.png',
-  './images/cast.png',
-  './images/staff.png',
-
-  './images/intro/top.png', // intro
-  './images/intro/bottom.png',
-
-  './images/news/noimage.png', // news
-
-  './images/movie/noimage.png', // movie
-
-  './images/character/amor.png', // character
-  './images/character/lolo.png',
-  './images/character/mitsuki.png',
-  './images/character/icon/amor.png',
-  './images/character/icon/lolo.png',
-  './images/character/icon/mitsuki.png',
-  './images/character/name/amor.png',
-  './images/character/name/lolo.png',
-  './images/character/name/mitsuki.png',
-  './images/character/pic/amor.jpg',
-  './images/character/pic/lolo.jpg',
-  './images/character/pic/mitsuki.jpg',
 ]
